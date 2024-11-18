d2ef3465d3fa8b0dabdb14fd291dea30b29d2c4d
verebes.tibor <verebes.tibor@28-DIAK9>
2024-11-18 12:07:10 +0100

    Add project files.

---
+++ berek2020/Berek.cs
@@ -0,0 +1,22 @@
+﻿namespace berek2020
+{
+    class Berek
+    {
+
+        public string Nev { get; set; }
+        public string Nem { get; set; }
+        public string Reszleg { get; set; }
+        public int Belepes { get; set; }
+        public int Ber { get; set; }
+
+        public Berek(string sor) 
+        {
+            string[] b = sor.Split(';');
+            Nev = b[0];
+            Nem = b[1];
+            Reszleg = b[2];
+            Belepes = Convert.ToInt32(b[3]);
+            Ber = Convert.ToInt32(b[4]);
+        }
+    }
+}
---
+++ berek2020/Program.cs
@@ -0,0 +1,45 @@
+﻿using berek2020;
+using System.Text;
+
+List<Berek> berek = [];
+using StreamReader sr = new("..\\..\\..\\src\\berek2020.txt", Encoding.UTF8);
+
+sr.ReadLine();
+
+while (!sr.EndOfStream)
+{
+    berek.Add(new(sr.ReadLine()));
+}
+
+Console.WriteLine($"3. feladat: Dolgozók száma: {berek.Count} fő");
+
+var f4 = berek
+    .Average(b => b.Ber);
+Console.WriteLine($"4. feladat: Bérek átlaga: {f4 / 1000:0.0} eFt");
+
+Console.Write("5. feladat: Kérem a részleg nevét: ");
+string reszleg = Console.ReadLine();
+
+int max = 0;
+for (int i = 0;i< berek.Count; i++)
+    if (berek[i].Reszleg == reszleg)
+    {
+        if (berek[i].Ber > berek[max].Ber) { max = i; }
+    }
+if (berek[max].Reszleg == reszleg)
+{
+    Console.WriteLine("6. feladat: A legtöbbet kereső dolgozó a megadott részlegen");
+    Console.WriteLine($"\tNév: {berek[max].Nev}");
+    Console.WriteLine($"\tNem: {berek[max].Nem}");
+    Console.WriteLine($"\tBelépés: {berek[max].Belepes}");
+    Console.WriteLine($"\tBér: {berek[max].Ber:0 000} Forint");
+}
+else Console.WriteLine("6. feladat: A megadott részleg nem létezik a cégnél!");
+
+var f7 = berek
+    .GroupBy(b => b.Reszleg);
+Console.WriteLine("7. feladat: Statisztika");
+foreach (var f in f7)
+{
+    Console.WriteLine($"\t {(f.Key)} - {(f.Count())} fő");
+}
